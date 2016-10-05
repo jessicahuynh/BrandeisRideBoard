eb6761fe91c38adc8d0f085324314bd82051368a
tim <tjhickey@brandeis.edu>
2016-10-05 09:06:05 -0400

    created a server startup function

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,10 @@
 Meteor.startup(function(){
-	
+	if (Info.find().fetch().length == 0) {
+		console.log("adding documents to the Info collection")
+		Info.insert({name:"Hillary",party:"Democratic"});
+		Info.insert({name:"Donald",party:"Republican"});
+		Info.insert({name:"Jill",party:"Green"});
+		Info.insert({name:"Gary",party:"Libertarian"});
+
+	}
 });
